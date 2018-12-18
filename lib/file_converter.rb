269b7a08d9891e0387f61e19eb04f9792f89dbbe
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-12-18 12:39:27 -0700

    added functionality to info_hash method

--- lib/file_converter.rb
+++ lib/file_converter.rb
@@ -4,13 +4,26 @@ class FileConverter
     @file_name = file_name
   end
 
-  def info_hash(file_name)
-    header_array = parse_csv(file_name)[0]
-    
-  end
-
   def parse_csv(file_name)
     CSV.readlines(file_name)
+  end
+
+
+  def info_hash(file_name)
+    full_hash = {}
+    full_array = parse_csv(file_name)
+    header_array = full_array.shift
+
+    full_array.each do |data_array|
+      index = 0
+      inner_hash = {}
+      data_array.each do |data|
+        inner_hash[header_array[index].to_sym] = data
+        index += 1
+      end
+      full_hash[data_array[0].to_sym] = inner_hash
+    end
+    full_hash
   end
 
 end
