d01113ff450b5f2dacd98184d5e1482a8ab19fa1
nagerz <znager@gmail.com>
2018-12-17 17:31:54 -0700

    Added to stat tracker test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,8 +1,13 @@
 class StatTracker
+  attr_reader :locations
 
   def initialize(locations)
     @locations = locations
+    #@game_info = file_converter.new(locations[:games]).game_info_converter
   end
 
+  def self.from_csv(locations)
+    StatTracker.new(locations)
+  end
 
 end
