3ae16485f2757981bef9a7007701faf0d9218479
Zach Leach <zachleach@techoverco.com>
2018-12-19 10:46:42 -0700

    Added class game and verified the test we wrote worked.

--- test/game_test.rb
+++ test/game_test.rb
@@ -6,7 +6,7 @@ require 'pry'
 class GameTest < Minitest::Test
 
   def setup
-    game_data_1 = {
+    @game_data_1 = {
                             :game_id=>"2012030221",
                             :season=>"20122013",
                             :type=>"P",
@@ -24,7 +24,7 @@ class GameTest < Minitest::Test
                             :venue_time_zone_tz=>"EDT"
                           }
 
-    game_data_2 = {
+    @game_data_2 = {
                             :game_id=>"2012030222",
                             :season=>"20122013",
                             :type=>"P",
@@ -43,7 +43,7 @@ class GameTest < Minitest::Test
                           }
 
 
-    game_data_3 = {
+    @game_data_3 = {
                             :game_id=>"2012030223",
                             :season=>"20122013",
                             :type=>"P",
@@ -62,13 +62,32 @@ class GameTest < Minitest::Test
                             }
 
 
-    @game_1 = Game.new(game_data_1)
-    @game_2 = Game.new(game_data_2)
-    @game_3 = Game.new(game_data_3)
+    @game_1 = Game.new(@game_data_1)
+    @game_2 = Game.new(@game_data_2)
+    @game_3 = Game.new(@game_data_3)
   end
 
   def test_it_exists
     assert_instance_of Game, @game_1
+  end
+
+  def test_it_has_attributes
+    assert_equal "2012030221", @game_1.game_id
+    assert_equal "20122013", @game_1.season
+    assert_equal "P", @game_1.type
+    assert_equal "2013-05-16", @game_1.date_time
+    assert_equal "3", @game_1.away_team_id
+    assert_equal "6", @game_1.home_team_id
+    assert_equal "2", @game_1.away_goals
+    assert_equal "3", @game_1.home_goals
+    assert_equal "home win OT", @game_1.outcome
+    assert_equal "left", @game_1.home_rink_side_start
+    assert_equal "TD Garden", @game_1.venue
+    assert_equal "/api/v1/venues/	null", @game_1.venue_link
+    assert_equal "America New_York", @game_1.venue_time_zone_id
+    assert_equal "-4", @game_1.venue_time_zone_offset
+    assert_equal "EDT", @game_1.venue_time_zone_tz
+
   end
 
 end
