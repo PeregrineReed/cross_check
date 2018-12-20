f7e14319f74530b8be1312ab8c2ac81be1d99114
Zach Leach <zachleach@techoverco.com>
2018-12-19 18:02:49 -0700

    Added test it has attributes in team_test.rb.

--- test/team_test.rb
+++ test/team_test.rb
@@ -42,24 +42,14 @@ class TeamTest < Minitest::Test
     assert_instance_of Team, @team_1
   end
 
-#   def test_it_has_attributes
-#     assert_equal "2012030221", @game_1.game_id
-#     assert_equal "20122013", @game_1.season
-#     assert_equal "P", @game_1.type
-#     assert_equal "2013-05-16", @game_1.date_time
-#     assert_equal "3", @game_1.away_team_id
-#     assert_equal "6", @game_1.home_team_id
-#     assert_equal 2, @game_1.away_goals
-#     assert_equal 3, @game_1.home_goals
-#     assert_equal "home win OT", @game_1.outcome
-#     assert_equal "left", @game_1.home_rink_side_start
-#     assert_equal "TD Garden", @game_1.venue
-#     assert_equal "/api/v1/venues/	null", @game_1.venue_link
-#     assert_equal "America New_York", @game_1.venue_time_zone_id
-#     assert_equal "-4", @game_1.venue_time_zone_offset
-#     assert_equal "EDT", @game_1.venue_time_zone_tz
-#
-#   end
+  def test_it_has_attributes
+    assert_equal "1", @team_1.team_id
+    assert_equal "23", @team_1.franchise_id
+    assert_equal "New Jersey", @team_1.short_name
+    assert_equal "Devils", @team_1.team_name
+    assert_equal "NJD", @team_1.abbreviation
+    assert_equal "/api/v1/teams/1", @team_1.link
+  end
 #
 #   def test_it_has_a_total_score
 #     assert_equal 5, @game_1.total_score
