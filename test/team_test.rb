0d5a5d9b46356c7e297069a096358ab49e8a93fe
Zach Leach <zachleach@techoverco.com>
2018-12-20 11:21:57 -0700

    Fixed indentation problems.

--- test/team_test.rb
+++ test/team_test.rb
@@ -7,31 +7,31 @@ class TeamTest < Minitest::Test
 
   def setup
     @team_data_1 = {
-                            :team_id=>"1",
-                            :franchise_id=>"23",
-                            :short_name=>"New Jersey",
-                            :team_name=>"Devils",
-                            :abbreviation=>"NJD",
-                            :link=>"/api/v1/teams/1"
-                            }
+      :team_id=>"1",
+      :franchise_id=>"23",
+      :short_name=>"New Jersey",
+      :team_name=>"Devils",
+      :abbreviation=>"NJD",
+      :link=>"/api/v1/teams/1"
+    }
 
     @team_data_2 = {
-                            :team_id=>"4",
-                            :franchise_id=>"16",
-                            :short_name=>"Philadelphia",
-                            :team_name=>"Flyers",
-                            :abbreviation=>"PHI",
-                            :link=>"/api/v1/teams/4"
-                            }
+      :team_id=>"4",
+      :franchise_id=>"16",
+      :short_name=>"Philadelphia",
+      :team_name=>"Flyers",
+      :abbreviation=>"PHI",
+      :link=>"/api/v1/teams/4"
+    }
 
     @team_data_3 = {
-                            :team_id=>"26",
-                            :franchise_id=>"14",
-                            :short_name=>"Los Angeles",
-                            :team_name=>"Kings",
-                            :abbreviation=>"LAK",
-                            :link=>"/api/v1/teams/26"
-                            }
+      :team_id=>"26",
+      :franchise_id=>"14",
+      :short_name=>"Los Angeles",
+      :team_name=>"Kings",
+      :abbreviation=>"LAK",
+      :link=>"/api/v1/teams/26"
+    }
 
     @team_1 = Team.new(@team_data_1)
     @team_2 = Team.new(@team_data_2)
@@ -49,5 +49,12 @@ class TeamTest < Minitest::Test
     assert_equal "Devils", @team_1.team_name
     assert_equal "NJD", @team_1.abbreviation
     assert_equal "/api/v1/teams/1", @team_1.link
+  end
+#A hash with key/value pairs
+#for each of the attributes of a team.
+  def test_it_has_team_info
+
+    hash = {:team_id => "1", :franchise_id=>"23", :short_name=>"New Jersey", :team_name=>"Devils", :abbreviation=>"NJD", :link=>"/api/v1/teams/1"}
+    assert_equal assert_equal hash, @team_1.team_info(@team_data_1)
   end
 end
