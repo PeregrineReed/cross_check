514e7f095105b72d124b4edfd6cdc046b931f62d
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-12-21 10:47:29 -0700

    Update for file converter refactor

--- lib/file_converter.rb
+++ lib/file_converter.rb
@@ -1,29 +1,32 @@
 class FileConverter
 
-  def initialize(file_name)
-    @file_name = file_name
+  attr_reader :file
+
+  def initialize(file)
+    @file = file
   end
 
-  def parse_csv(file_name)
-    CSV.readlines(file_name)
+  def parse_csv(file)
+    CSV.open(file, headers: true, header_converters: :symbol)
   end
 
+  def info_hash
+    # full_hash = {}
+    csv_contents = parse_csv(file)
+      
+    csv_contents.each do |row|
 
-  def info_hash(file_name)
-    full_hash = {}
-    full_array = parse_csv(file_name)
-    header_array = full_array.shift
-
-    full_array.each do |data_array|
-      index = 0
-      inner_hash = {}
-      data_array.each do |data|
-        inner_hash[header_array[index].to_sym] = data
-        index += 1
-      end
-      full_hash[data_array[0].to_sym] = inner_hash
     end
-    full_hash
+    # full_array.each do |data_array|
+    #   index = 0
+    #   inner_hash = {}
+    #   data_array.each do |data|
+    #     inner_hash[header_array[index].to_sym] = data
+    #     index += 1
+    #   end
+    #   full_hash[data_array[0].to_sym] = inner_hash
+    # end
+    # full_hash
   end
 
 end
