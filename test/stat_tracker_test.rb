acb1a1652e9a0a979e32b9726993571673429138
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-12-24 13:31:49 -0700

    Added test for biggest blowout

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -74,4 +74,8 @@ class StatTrackerTest < Minitest::Test
     assert_equal 3, @stat_tracker.lowest_total_score
   end
 
+  def test_it_can_determine_biggest_blowout
+    assert_equal 3, @stat_tracker.biggest_blowout
+  end
+
 end
