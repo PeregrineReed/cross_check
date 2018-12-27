7255cff0e3e6e4017dc95ccfa42ad123cd928ed9
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-12-26 20:12:05 -0700

    Added method for teams by id

--- lib/stat_sorter.rb
+++ lib/stat_sorter.rb
@@ -1,6 +1,17 @@
 class StatSorter
 
+  attr_reader :teams
+
   def initialize(games, teams)
+    @teams = teams
   end
-  
+
+  def teams_by_id
+    teams = {}
+    @teams.each do |team|
+      teams[team.team_id] = team
+    end
+    teams
+  end
+
 end
--- test/stat_sorter_test.rb
+++ test/stat_sorter_test.rb
@@ -4,41 +4,41 @@ require './lib/stat_sorter'
 class StatSorterTest < Minitest::Test
 
   def setup
-    game_1 = mock
-    game_2 = mock
-    game_3 = mock
-    game_4 = mock
-    game_5 = mock
+    @game_1 = mock
+    @game_2 = mock
+    @game_3 = mock
+    @game_4 = mock
+    @game_5 = mock
 
-    games = [
-      game_1, # teams 1, 2
-      game_2, # teams 3, 4
-      game_3, # teams 5, 1
-      game_4, # teams 2, 3
-      game_5, # teams 4, 5
+    @games = [
+      @game_1, # teams 1, 2
+      @game_2, # teams 3, 4
+      @game_3, # teams 5, 1
+      @game_4, # teams 2, 3
+      @game_5, # teams 4, 5
     ]
 
-    team_1 = mock
-    team_2 = mock
-    team_3 = mock
-    team_4 = mock
-    team_5 = mock
+    @team_1 = mock
+    @team_2 = mock
+    @team_3 = mock
+    @team_4 = mock
+    @team_5 = mock
 
-    teams = [
-      team_1,
-      team_2,
-      team_3,
-      team_4,
-      team_5
+    @teams = [
+      @team_1,
+      @team_2,
+      @team_3,
+      @team_4,
+      @team_5
     ]
 
-    team_1.stubs(:team_id => "1")
-    team_2.stubs(:team_id => "2")
-    team_3.stubs(:team_id => "3")
-    team_4.stubs(:team_id => "4")
-    team_5.stubs(:team_id => "5")
+    @team_1.stubs(:team_id => "1")
+    @team_2.stubs(:team_id => "2")
+    @team_3.stubs(:team_id => "3")
+    @team_4.stubs(:team_id => "4")
+    @team_5.stubs(:team_id => "5")
 
-    @stat_sorter = StatSorter.new(games, teams)
+    @stat_sorter = StatSorter.new(@games, @teams)
   end
 
   def test_it_exists
@@ -47,11 +47,11 @@ class StatSorterTest < Minitest::Test
 
   def test_it_can_group_teams_by_id
     expected = {
-      "1" => team_1,
-      "2" => team_2,
-      "3" => team_3,
-      "4" => team_4,
-      "5" => team_5
+      "1" => @team_1,
+      "2" => @team_2,
+      "3" => @team_3,
+      "4" => @team_4,
+      "5" => @team_5
     }
 
     assert_equal expected, @stat_sorter.teams_by_id
