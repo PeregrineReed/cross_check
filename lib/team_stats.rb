6dadd7889e28960dbd1f20cd3e5f8e348d5d3f1c
nagerz <znager@gmail.com>
2018-12-30 00:51:44 -0700

    Added seasonal_summary methods to team_stats.

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -1,8 +1,29 @@
-class TeamStats
-  attr_reader :teams
+class TeamStats < StatSorter
+  attr_reader :games,
+              :teams
 
-  def initialize(teams)
-    @teams = teams
+  def initialize(games, teams)
+    super(games, teams)
   end
+
+  def seasonal_summary(team_id)
+    team = teams_by_id[team_id]
+    seasonal_summary = {}
+    team.seasons.each do |season|
+      seasonal_summary[season] = single_seasonal_summary(team, season)
+    end
+    seasonal_summary
+  end
+
+  def single_seasonal_summary(team, season)
+    summary = team.seasons[season].season_summary
+    summary[:preseason][:average_goals_scored] = (team.seasons[season].preseason[:goals] / team.seasons[season].preseason[:games].to_f).round(2)
+    summary[:preseason][:average_goals_against] = (team.seasons[season].preseason[:goals_against] / team.seasons[season].preseason[:games].to_f).round(2)
+    summary[:regular_season][:average_goals_scored] = (team.seasons[season].regular[:goals] / team.seasons[season].regular[:games].to_f).round(2)
+    summary[:regular_season][:average_goals_against] = (team.seasons[season].regular[:goals_against] / team.seasons[season].regular[:games].to_f).round(2)
+    return summary
+  end
+
+
 
 end
